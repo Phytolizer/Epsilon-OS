b063e894cfdd7372b6ff04bf5f4578377786ee39
Phytolizer <kyle.coffey@tamu.edu>
2021-10-26 05:29:24 -0500

    modify appearance of script

--- make.rb
+++ make.rb
@@ -4,6 +4,8 @@ COMPILER = ENV['CC'] || 'i686-elf-gcc'
 ASSEMBLER = ENV['ASM'] || 'i686-elf-as'
 LINKER = ENV['LD'] || 'i686-elf-ld'
 
+exe_length = [COMPILER.length, ASSEMBLER.length, LINKER.length].max + 3
+
 SOURCES = File.read('c.list').split
 ASM = File.read('s.list').split
 BUILD = 'builddir'
@@ -26,24 +28,26 @@ end
 
 exit 1 unless ok
 
-# build C code
+puts "-- Build #{'C'.yellow} code"
 SOURCES.each do |src|
-  puts '%15s : %30s => %-30s' % [COMPILER, src.blue, src.to_obj.green]
+  puts "%#{exe_length}s : %s => %-30s" % [COMPILER, src.blue, src.to_obj.green]
   ok = system "#{COMPILER} -c #{src} -o #{src.to_obj} -ffreestanding"
   exit 1 unless ok
 end
-# build ASM code
+puts "-- Build #{'assembly'.yellow} code"
 ASM.each do |asm|
-  puts '%15s : %30s => %-30s' % [ASSEMBLER, asm.blue, asm.to_obj.green]
+  puts "%#{exe_length}s : %s => %-30s" % [ASSEMBLER, asm.blue, asm.to_obj.green]
   ok = system "#{ASSEMBLER} #{asm} -o #{asm.to_obj}"
   exit 1 unless ok
 end
 
-# link
-puts "%15s : [#{OBJS.map(&:blue).join(', ')}] => #{FULL_KERNEL.green}" % [LINKER]
+puts "-- #{'Link'.yellow}"
+puts "%#{exe_length}s : [#{OBJS.map(&:blue).join(', ')}] => #{FULL_KERNEL.green}" % [LINKER]
 ok = system "#{LINKER} -T link.ld -nostdlib #{OBJS.join(' ')} -o #{FULL_KERNEL}"
 exit 1 unless ok
 
 # check bootable
+print "Check image..."
 ok = system "grub-file --is-x86-multiboot #{FULL_KERNEL}"
+puts ok ? "OK".green : "NOT OK".red
 exit 1 unless ok
